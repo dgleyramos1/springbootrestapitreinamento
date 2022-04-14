15383c935d9a4491388964ee5f9967a4e3b395cc
Dgley Ramos <dgleysilva@gmail.com>
2022-04-14 16:57:05 -0300

    Criando método GET para listar todos os usuários

--- src/main/java/br/com/springboot/dev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/dev_treinamento/controllers/GreetingsController.java
@@ -1,13 +1,19 @@
 package br.com.springboot.dev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.dev_treinamento.model.Usuario;
 import br.com.springboot.dev_treinamento.repository.UsuarioRepository;
 
 /**
@@ -21,14 +27,34 @@ public class GreetingsController {
 	@Autowired /*IC - ICD - Injeção de dependencia*/
 	private UsuarioRepository usuarioRepository;
 	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
+	
+	
+	
+	/*Método GET, para listar todos os usúarios*/
+	@GetMapping(value = "usuarios")
+	@ResponseBody /* Retorna os dados para o corpo da resposta*/
+    public ResponseEntity<List<Usuario>> listUsuarios(){
+		
+		List<Usuario> usuarios = usuarioRepository.findAll();
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
