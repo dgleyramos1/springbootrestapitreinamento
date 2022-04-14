bc477538a6ef4e5e4a4248d10fa79fdd283edca9
Dgley Ramos <dgleysilva@gmail.com>
2022-04-14 18:27:30 -0300

    Criando método POST, para cadastrar usuário

--- src/main/java/br/com/springboot/dev_treinamento/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/dev_treinamento/controllers/GreetingsController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.dev_treinamento.model.Usuario;
@@ -30,14 +28,23 @@ public class GreetingsController {
 	
 	
 	
-	/*Método GET, para listar todos os usúarios*/
+	/*Método GET, para listar todos os usuários*/
 	@GetMapping(value = "usuarios")
 	@ResponseBody /* Retorna os dados para o corpo da resposta*/
-    public ResponseEntity<List<Usuario>> listUsuarios(){
+    public ResponseEntity<List<Usuario>> listarUsuarios(){
 		
 		List<Usuario> usuarios = usuarioRepository.findAll();
     	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
     }
+	
+	
+	/*Método POSt, salvar usuáiro no banco de dados*/
+	@PostMapping(value = "cadastrar")
+	@ResponseBody
+	public ResponseEntity<Usuario> salvarUsuario(@RequestBody Usuario usuario){
+		Usuario user = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
 }
 
 
